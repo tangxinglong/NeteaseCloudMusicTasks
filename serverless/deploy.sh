21202ed14e971c938d9c1627f766d30008d7002c
tangxinglong <69668388+tangxinglong@users.noreply.github.com>
2024-08-23 16:05:20 +0800

    Update deploy.sh

--- serverless/deploy.sh
+++ serverless/deploy.sh
@@ -47,7 +47,7 @@ else
 fi
 
 echo "开始安装 ServerlessFramework"
-sudo npm install -g serverless@3 >>/dev/null
+sudo npm install -g serverless >>/dev/null
 sudo mkdir tmp/
 shopt -s extglob
 sudo mv !(tmp|serverless|public|code|.github|.git|ql_update.py|__pycache__|Dockerfile|.dockerignore|scheduler.py) tmp
